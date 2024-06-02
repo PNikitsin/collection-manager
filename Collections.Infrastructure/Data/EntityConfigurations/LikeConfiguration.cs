ef005c5b75ecbd27288d54a267563c9ed479834c
Pavel Nikitsin <pnikitinbox@gmail.com>
2024-06-03 00:35:09 +0300

    Added likes configuration.

---
+++ Collections.Infrastructure/Data/EntityConfigurations/LikeConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Collections.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Collections.Infrastructure.Data.EntityConfigurations
+{
+    public class LikeConfiguration : IEntityTypeConfiguration<Like>
+    {
+        public void Configure(EntityTypeBuilder<Like> builder)
+        {
+            builder.ToTable("Likes");
+
+            builder.HasKey(like => like.Id);
+
+            builder.Property(like => like.Username).IsRequired();
+        }
+    }
+}
